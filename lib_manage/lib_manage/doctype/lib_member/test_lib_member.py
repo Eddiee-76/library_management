6a1160176bdbe87b8a9e4b3935854f7bcffb72b0
Adil <naikadil75@gmail.com>
2024-01-18 01:14:11 +0530

    final

---
+++ lib_manage/lib_manage/doctype/lib_member/test_lib_member.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Eddie and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLib_member(FrappeTestCase):
+	pass
---
+++ lib_manage/lib_manage/doctype/library_settings/test_library_settings.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2024, Eddie and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestLibrarysettings(FrappeTestCase):
+	pass
